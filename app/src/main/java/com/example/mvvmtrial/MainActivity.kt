f7de21c32de9dbbdc85e1f513c4ff1e8968616cd
pinocchio22 <vviian.2@gmail.com>
2023-02-28 15:57:38 +0900

    live data

--- app/src/main/java/com/example/mvvmtrial/MainActivity.kt
+++ app/src/main/java/com/example/mvvmtrial/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.mvvmtrial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.example.mvvmtrial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var liveText = MutableLiveData<String>()
+    var count = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.apply {
+            //(중요) DataBinding에 LifeCycleOwner를 지정해줘야 LiveData가 실시간적으로 변경됨
+            lifecycleOwner = this@MainActivity
+            //xml파일에서 선언한 activity (이름이 같아야함!)
+            activity = this@MainActivity
+        }
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        liveText.observe(this, Observer {
+            Log.e("YMC", "observe: ${binding.mButton.text}")
+        })
+
+
+        //DataBinding 예제
+        //xml파일에서 설정한 value값은 kotlin에서 MutableLiveData로 선언이 되있다! - 변경을 바로 확인하기 위해서!
+        binding.mButton.setOnClickListener {
+            liveText.value = "DataBinding으로 변경된 textview ${count++}"
+//            binding.invalidateAll() //data가 변한 후 연결된 view들에게 변화를 알려주는 함수
+        }
     }
 }
